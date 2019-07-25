732845d0b03fc3fcdc1c7c24faed4779bd6cde2e
Jacob <jacobzhou@g.ucla.edu>
2019-07-25 15:10:01 -0700

    Poly and Linear

--- model.py
+++ model.py
@@ -1,25 +1,39 @@
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-dataset = pd.read_csv('Poisson_data_no_metal_07192019.csv')
-X = dataset.iloc[:,6:187].values
-y = dataset.iloc[:,189].values
-
 from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X,y)
-
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+
+dataset = pd.read_csv('Non_metal_07252019.csv')
+X = dataset.iloc[:,7:188].values
+y = dataset.iloc[:,190].values
+
+#linear regression
+
+# lin_reg = LinearRegression()
+# lin_reg.fit(X,y)
+# pred = lin_reg.predict(X)
+# print(y)
+# print(pred)
+# plt.plot(y,pred,'ro',markersize = 1)
+# plt.axis([0,0.5,0,0.5])
+# plt.plot([0,0.1,0.2,0.3,0.4,0.5],[0,0.1,0.2,0.3,0.4,0.5])
+# plt.show()
+
+
+#polynomial regression
+poly_reg = PolynomialFeatures(degree = 3)
 X_poly = poly_reg.fit_transform(X)
 poly_reg.fit(X_poly, y)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
+pred = lin_reg_2.predict(X_poly)
 
-pred = lin_reg.predict(X)
-plt.plot(y,pred,'ro')
+plt.plot(y,pred,'ro',markersize = 1)
 plt.axis([0,0.5,0,0.5])
 plt.plot([0,0.1,0.2,0.3,0.4,0.5],[0,0.1,0.2,0.3,0.4,0.5])
+plt.show()
+
+
+
+
